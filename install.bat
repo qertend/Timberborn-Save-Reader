2c60f1ace9d480a2589e4ee82676633fbf2e13cb
qertend <szucslevente607@gmail.com>
2024-04-20 01:36:08 +0200

    installer now creates savelocation txt

--- install.bat
+++ install.bat
@@ -10,6 +10,8 @@ ECHO Node.js not found, installing...
 winget install "Node.js"
 ::download node modules
 npm i
+::init Timberborn_save_location.txt
+echo "C:/Users/[user]/Documents/Timberborn/Saves/" > ".\Timberborn_save_location.txt"
 GOTO finish
 
 :nodeInstalled
